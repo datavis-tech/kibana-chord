651ed9a9e4d3cf2246a85d3d972ac6472081d198
Curran <curran.kelleher@gmail.com>
2016-07-08 15:51:23 +0530

    Add chord.js plugin scaffold

---
+++ public/chord.js
@@ -0,0 +1,28 @@
+define(function(require) {
+  var module = require("ui/modules").get("kibana-chord");
+
+  module.controller("ChordController", function($scope) {
+    $scope.foo = "Hello Kibana!";
+  });
+
+  function ChordProvider(Private) {
+    var TemplateVisType = Private(require("ui/template_vis_type/TemplateVisType"));
+
+    return new TemplateVisType({
+      name: "chord",
+      title: "Chord Diagram",
+      icon: "fa-pie-chart", // TODO make this look like a chord diagram
+      description: "Visualize network flows with a Chord Diagram.",
+      requiresSearch: false,
+      template: require("plugins/kibana-chord/chord.html"),
+      params: {
+        editor: require("plugins/kibana-chord/chord-editor.html")
+      }
+    });
+  }
+
+  require("ui/registry/vis_types").register(ChordProvider);
+
+  return ChordProvider;
+
+});
