c2a69aea0a4f00bd5ed8c8525be008e11cbff84b
Curran <curran.kelleher@gmail.com>
2016-07-16 15:19:56 +0530

    Add build step to upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,9 @@
 # This script will re-install the latest version of the plugin and launch Kibana.
 git pull
 npm install
-cd ..
+cd d3
+sh build.sh
+cd ../..
 zip -r kibana-chord.zip kibana-chord -x "*.git*" "*node_modules*"
 /opt/kibana/bin/kibana plugin --remove kibana-chord
 /opt/kibana/bin/kibana plugin --install kibana-chord -u file://`pwd`/kibana-chord.zip
