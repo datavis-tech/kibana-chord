efd806ec977dac5084afca3d22fce3324229b0b8
curran <curran.kelleher@gmail.com>
2016-08-01 16:47:20 -0400

    Add init function for custom middleware. Towards #26

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 module.exports = function(kibana) {
   return new kibana.Plugin({
+
+    // Export the front-end visualization definition.
     uiExports: {
       visTypes: [
         "plugins/kibana-chord/chord"
       ]
+    },
+
+    // Define our custom server-side middleware for
+    // custom ElasticSearch queries.
+    init: function (server, options) {
     }
+
   });
 };
